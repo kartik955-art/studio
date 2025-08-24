c1abf77f245dcbc1e131a947458a285d9544efce
CSR TOONS <csrtoons00@gmail.com>
2025-08-24 10:31:41 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="mx-aut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     <div className="flex min-h-screen w-full flex-col">
       <Header />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <div className="mx-auto grid w-full max-w-4xl gap-2">
+        <div className="mx-auto grid w-full max-w-4xl gap-2 text-center">
           <h1 className="text-3xl font-semibold font-headline">
             Welcome to Nexora
           </h1>
